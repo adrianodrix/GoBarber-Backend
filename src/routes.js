26a5362182dcd75183941b8e959bb0a7f5c0b264
Adriano Santos <adrianodrix@gmail.com>
2019-09-10 17:50:03 -0300

    appointments and schedule

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import MulterConfig from './config/multer';
 
 import AuthMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
+const upload = multer(MulterConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello!' });
@@ -16,5 +24,13 @@ routes.post('/users', UserController.store);
 
 routes.use(AuthMiddleware);
 routes.put('/users/me', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
